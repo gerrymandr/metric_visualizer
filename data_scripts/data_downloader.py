13d95d74f69f69df01f1dd926cc155a09322583f
solbrigm <solbrigm@gmail.com>
2017-08-10 12:08:47 -0700

    Updated data_downloader, it now works on MN congressional data

--- data_scripts/data_downloader.py
+++ data_scripts/data_downloader.py
@@ -1,11 +1,11 @@
 import shutil
-import urllib2
+from urllib.request import urlopen
 import zipfile
 import os
 from contextlib import closing
 
 
-def download_data(base_url, file_list, destination_dir):
+def download_data(url, file_name, destination_dir):
     """Downloads file_name.zip from base_url.file_name.zip, downoloads to destination_dir and extracts"""
 
     try:
@@ -14,24 +14,28 @@ def download_data(base_url, file_list, destination_dir):
         print('Did not find directory, creating new directory {}'.format(destination_dir))
         os.mkdir(destination_dir)
 
+    dest_path = os.path.join(destination_dir, file_name+'.zip')
 
-    url_list = [base_url.format(file_name) for file_name in file_list]
-    for url in url_list:
-        print('downloading data from {0}'.format(url))
-        with closing(urllib2.urlopen(url)) as r:
-            dest_path = os.path.join(destination_dir, file_name)
-            with open(dest_path, 'wb') as f:
-                shutil.copyfileobj(r, f)
+    print('downloading data from {0}, writing to {1}'.format(url, dest_path))
+    with closing(urlopen(url)) as r:
+        with open(dest_path, 'wb') as f:
+            shutil.copyfileobj(r, f)
 
     zip_ref = zipfile.ZipFile(dest_path, 'r')
-    print('')
-    zip_ref.extractall('RawData')
+    print('extracting data to ')
+    zip_ref.extractall(os.path.join('RawData', file_name))
     zip_ref.close()
 
 
 if __name__ == "__main__":
-    file_list = ['C2012', 'C2002', 'C1994']
-    base_url = 'ftp://ftp.commissions.leg.state.mn.us/pub/gis/Redist2010/Plans/congress/{0}/{0}.zip'
-    download_data(base_url, file_list, 'RawData')
+    destination_folder = os.path.join(os.getcwd(), 'RawData')
+    print(destination_folder)
+    shutil.rmtree(destination_folder, ignore_errors=True)
+    file_urls = {'C2012': 'ftp://ftp.commissions.leg.state.mn.us/pub/gis/Redist2010/Plans/congress/C2012/C2012.zip',
+                 'C2002': 'ftp://ftp.commissions.leg.state.mn.us/pub/gis/shape/C2002.zip',
+                 'C1994': 'ftp://ftp.commissions.leg.state.mn.us/pub/gis/shape/con94.zip'
+    }
+    for k, v in file_urls.items():
+        download_data(v, k, 'RawData')
 
 
