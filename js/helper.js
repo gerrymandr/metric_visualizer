916d31fdf81b1869de789277b1308ddcbc0ee984
Chris Cantey <ccantey@wis.edu>
2017-08-11 14:10:00 -0500

    looking good

--- js/helper.js
+++ js/helper.js
@@ -3,87 +3,72 @@ $( document ).ready(function() {
     initialize();
     $('#clear').hide();
 
-    // $('.mapboxgl-ctrl-top-right').affix({
-    //   offset: {
-    //     top: 210
-    //   }
-    // });
+    $('.mapboxgl-ctrl-top-right').affix({
+      offset: {
+        top: 210
+      }
+    });
     $('#clear').on('click', function(){
     removeLayers('all');
 
   })
 
-    $('.election-navigation-a').on('click', function(e){
-    	e.preventDefault();
-      $('#clear').hide();
-      //remove previous selections map methods give an example of how one would toggle layers
-      document.getElementById('features').innerHTML = "";
-      // map.removeLayer("2012results-"+ activeTab.geography);
-      // map.removeLayer("2012results-"+ activeTab.geography+"-hover");
-      // map.setLayoutProperty(activeTab.geography + '-symbols', 'visibility', 'none');
-      // map.setLayoutProperty(activeTab.geography + '-lines', 'visibility', 'none');
-    	$('.election-navigation-a').removeClass('active');
-      
-      //add new selections
-    	$(this).addClass('active');
-    	activeSelect.selection = $(this).data('district');
-      activeSelect.geography = $(this).data('geography');
-      activeSelect.name = $(this).data('name');
-    	console.log(activeSelect);
-    })
-
-    $('#dropyear').on('change', function(e){
+$('.election-navigation-a').on('click', function(e){
       e.preventDefault();
+      //remove previous layers
+      $('#results').html("");
       $('#clear').hide();
-      //remove previous selections map methods give an example of how one would toggle layers
-      document.getElementById('features').innerHTML = "";
-      map.removeLayer(activeSelect.geography+"-"+activeSelect.year);
-      map.removeLayer(activeSelect.geography+"-highlighted-"+activeSelect.year);
-      spliceArray(activeSelect.geography+"-"+activeSelect.year);
-      spliceArray(activeSelect.geography+"-highlighted-"+activeSelect.year);
+        map.removeLayer(activeSelect.geography+"-"+activeSelect.year);
+        map.removeLayer(activeSelect.geography+"-highlighted-"+activeSelect.year);
+        spliceArray(activeSelect.geography+"-"+activeSelect.year);
+        spliceArray(activeSelect.geography+"-highlighted-"+activeSelect.year);
 
-      activeSelect.year = parseInt($(this).val());
-      // ['all', ["in", "DISTRICT", ""], ['==','Year',activeSelect.year]]
+      $('.election-navigation-a').removeClass('active');
+        
+      //add new selections
+      $(this).addClass('active');
+      activeSelect.selection = $(this).data('district');
+      activeSelect.geography = $(this).data('geography');
+      activeSelect.name = $(this).data('name');        
 
         var layer = [
-  [activeSelect.geography,'fill', ['==','Year',activeSelect.year],{"fill-color": {"type":activeSelect.paintType,"property": activeSelect.paintProperty,"stops": activeSelect.paintStops}, "fill-outline-color": "#fff","fill-opacity":0.75}],
-  [activeSelect.geography+"-highlighted", 'fill',['all', ["in", "DISTRICT", ""], ['==','Year',activeSelect.year]],{"fill-color": 'brown',"fill-outline-color": "#fff","fill-opacity":1}]
-    ];
+          [activeSelect.geography,'fill', ['all', ['==', 'year', activeSelect.year], ['==', 'FeatType', activeSelect.geography]],{"fill-color": {"type":activeSelect.paintType,"property": activeSelect.paintProperty,"stops": activeSelect.paintStops}, "fill-outline-color": "#fff","fill-opacity":0.75}],
+          [activeSelect.geography+"-highlighted", 'fill',['all', ["in", "District", ""], ['==', 'FeatType', activeSelect.geography],['==','Year',activeSelect.year]],{"fill-color": '#ff6600',"fill-outline-color": "#fff","fill-opacity":1}]
+        ];
 
-  layer.forEach(addLayer)
+        layer.forEach(addLayer) 
+    });
 
+    $('#dropyear').on('change', function(e){
+        e.preventDefault();
+        $('#clear').hide();
+        //remove previous selections map methods give an example of how one would toggle layers
+        $('#results').html("");
+        map.removeLayer(activeSelect.geography+"-"+activeSelect.year);
+        map.removeLayer(activeSelect.geography+"-highlighted-"+activeSelect.year);
+        spliceArray(activeSelect.geography+"-"+activeSelect.year);
+        spliceArray(activeSelect.geography+"-highlighted-"+activeSelect.year);
+
+        activeSelect.year = parseInt($(this).val());
+        var layer = [
+          [activeSelect.geography,'fill', ['all', ['==', 'year', activeSelect.year], ['==', 'FeatType', activeSelect.geography]],{"fill-color": {"type":activeSelect.paintType,"property": activeSelect.paintProperty,"stops": activeSelect.paintStops}, "fill-outline-color": "#fff","fill-opacity":0.75}],
+          [activeSelect.geography+"-highlighted", 'fill',['all', ["in", "District", ""], ['==', 'FeatType', activeSelect.geography],['==','Year',activeSelect.year]],{"fill-color": '#ff6600',"fill-outline-color": "#fff","fill-opacity":1}]
+        ];
+
+        layer.forEach(addLayer);
     });
 
     $('#dropcompactness').on('change', function(e){
       e.preventDefault();
       activeSelect.paintProperty = $(this).val();
-      if(activeSelect.paintProperty == 'measure1'){
-        activeSelect.paintStops = [[0, '#ffffcc'],[1, '#a1dab4']];
-      } else {
-        activeSelect.paintStops = [[0, '#ffffcc'],[.25, '#a1dab4'],[.50, '#41b6c4'],[.75, '#2c7fb8'], [1, '#253494']];
-      }
+      
+      // activeSelect.paintStops = [[0, '#ffffcc'],[.25, '#a1dab4'],[.50, '#41b6c4'],[.75, '#2c7fb8'], [1, '#253494']];
+      
       
       map.setPaintProperty(activeSelect.geography+"-"+activeSelect.year, 
                           "fill-color", {"type":activeSelect.paintType,
                                             "property": activeSelect.paintProperty,
-                                            "stops": activeSelect.paintStops})//ults', filter: ['has','COUNTYNAME']})
-
-      //remove previous selections map methods give an example of how one would toggle layers
-      // document.getElementById('features').innerHTML = "";
-      // map.removeLayer(activeSelect.geography+"-"+activeSelect.year);
-      // map.removeLayer(activeSelect.geography+"-highlighted-"+activeSelect.year);
-      // spliceArray(activeSelect.geography+"-"+activeSelect.year);
-      // spliceArray(activeSelect.geography+"-highlighted-"+activeSelect.year);
-
-      // activeSelect.paintProperty = $(this).val();
-
-  //       var layer = [
-  // [activeSelect.geography,'fill', ['==','Year',activeSelect.year],{"fill-color": {"type":activeSelect.paintType,"property": activeSelect.paintProperty,"stops": activeSelect.paintStops}, "fill-outline-color": "#fff","fill-opacity":0.75}],
-  // [activeSelect.geography+"-highlighted", 'fill',["in", "DISTRICT", ""],{"fill-color": 'brown',"fill-outline-color": "#fff","fill-opacity":1}]
-  //   ];
-
-  // layer.forEach(addLayer)
-
+                                            "stops": activeSelect.paintStops});
     })
 
   //mousemove is too slow
@@ -119,30 +104,6 @@ $( document ).ready(function() {
 
 
 
-    $('.election-navigation-a').on('click', function(e){
-      e.preventDefault();
-      //remove previous layers
-      $('#clear').hide();
-      // document.getElementById('precinct-header').innerHTML = "";
-      // document.getElementById('precinct-results').innerHTML = "";
-      // map.removeLayer("2016results-"+ activeTab.geography);
-      // map.removeLayer("2016results-"+ activeTab.geography+"-hover");
-      // spliceArray("2016results-"+ activeTab.geography);
-      // spliceArray("2016results-"+ activeTab.geography+"-hover");
-      // map.setLayoutProperty(activeTab.geography + '-symbols', 'visibility', 'none');
-      // map.setLayoutProperty(activeTab.geography + '-lines', 'visibility', 'none');
-      //remove any vtd selection
-      // map.setFilter("2016results-vtd", ['all', ['==', 'UNIT', 'vtd'], ["!=", "VTD",'any']]);
-      // map.setFilter("2016results-vtd-hover", ['all', ['==', 'UNIT', 'vtd'], ["==", "VTD",'all']]);
-
-      $('.election-navigation-a').removeClass('active');
-        
-      //add new selections
-      $(this).addClass('active');
-      activeTab.selection = $(this).data('district');
-      activeTab.geography = $(this).data('geography');
-      activeTab.name = $(this).data('name');
-      changeData(activeTab);
-    });
+    
 
  }); //end ready
