adbcd409fd87c74f3345dc4ea5ead92c89eafaa3
ccantey <cantey0701@gmail.com>
2017-08-14 09:55:05 -0500

    clean app.js

--- js/app.js
+++ js/app.js
@@ -40,56 +40,47 @@ function initialize(){
     var nav = new mapboxgl.NavigationControl({position: 'top-right'}); // position is optional
     map.addControl(nav);
 
-    // geocoder = new google.maps.Geocoder; //ccantey.dgxr9hbq
-    // geocoder = new MapboxGeocoder({
-    //     accessToken: mapboxgl.accessToken
-    // });
-    // window.geocoder = geocoder;
-
-    //map.addControl(geocoder);
-	 map.on('load', function () {
+    map.on('load', function () {
       // add vector source:
       map.addSource('mnleg_cng', {
           type: 'vector',
           url: 'mapbox://mggg.cj689nea60dqo2qryo5m6zsj4-0z2dc'
       });
 
-     var layers = [
-            //name, minzoom, maxzoom, filter, paint fill-color, stops, paint fill-opacity, stops            
+      var layers = [
+          //create layers array to load multiple layers
+          //see https://www.mapbox.com/mapbox-gl-js/style-spec/#layers-fill            
           [
-            activeSelect.geography,                  //layers[0] = id
-            'fill',                     //layer[1]            
-            ['all', ['==', 'Year', 2012], ["==", "FeatType", activeSelect.geography]],             //layers[2] = filter
-            {"fill-color": {              //layers[3] = paint object
+            activeSelect.geography,                   //layers[0] = id
+            'fill',                                   //layer[1]            
+            ['all', ['==', 'Year', 2012], ["==", "FeatType", activeSelect.geography]],  //layers[2] = filter
+            {"fill-color": {                          //layers[3] = paint object
                 "type":activeSelect.paintType,
                 "property": activeSelect.paintProperty,
                 "stops": activeSelect.paintStops
                 }, 
             "fill-outline-color": "#fff",
                   "fill-opacity":0.75
-            }], 
+            }],
+            //create {layer}-highlighted layer to toggle selected districts in app
             [activeSelect.geography+"-highlighted", 'fill',["in", "District", ""],{"fill-color": '#ff6600',"fill-outline-color": "#fff","fill-opacity":1}]]      
 
         layers.forEach(addLayer);
-  });//end map on load
+    });//end map on load
 } //end initialize
 
-function addLayer(layer) {
-             
+function addLayer(layer) {             
            map.addLayer({
-            "id": layer[0] +'-'+ activeSelect.year,
+            "id": layer[0] +'-'+ activeSelect.year, //layer names take the form: geography-year, ex: cng-2012
             "type": layer[1],
             "source": "mnleg_cng",
             "source-layer": "final", //layer name in studio
-            // "minzoom":layer[1],
-            // 'maxzoom': layer[2],
             'filter': layer[2],
             "layout": {},
             "paint": layer[3],
-           }, 'waterway-label');
+           }, 'waterway-label'); //place above this layer (within mapbox://styles/mggg/cj66qmslq7fh82ss33qbqlxoc basemap style)
 
-           layersArray.push(layer[0] +'-'+ activeSelect.year);
-           
+           layersArray.push(layer[0] +'-'+ activeSelect.year);           
 };
 
 //remove layersArray element per 0.22.0
@@ -109,14 +100,11 @@ function mapResults(feature){
           break;
       case activeSelect.geography+"-highlighted-"+activeSelect.year:
           break;
-      default:
-          // map.setFilter("2016results-"+activeTab.geography, ['all', ['==', 'UNIT', activeTab.geography], ["!=", activeTab.name, feature.properties[activeTab.name]]]);
-          //   map.setFilter("2016results-"+activeTab.geography+"-hover", ['all', ['==', 'UNIT', activeTab.geography], ["==", activeTab.name, feature.properties[activeTab.name]]]);
     }
 }
 
 function showResults(activeSelect, featureProperties){
-  console.log(activeSelect)
+  // console.log(activeSelect)
   var content = '';
   var header ='';
   var district = '';
@@ -126,42 +114,18 @@ function showResults(activeSelect, featureProperties){
     geography:activeSelect.geography
   };
   
-  // var attributeMap = {'AsianDisab':"Asian Disabled", "BlackDisab":"Black Disabled","LatinoDisa":"Latino Disabled", 'Native_A_1':"Native American Disabled","WhiteDisab":"White Disabled",
-  //           'FemHHPov':"Female Head Household Poverty",
-  //                       'AsianEmplo':"Asian Employed",'BlackEmplo':"Black Empolyed","LatinoEmpl":"Latino Employed","Native_Ame":"Native American Employed",'WhiteEmplo':"White Emplyed",
-  //                     'MNTaxes':"MN Taxes",'TotalIncom':"Total Income"}
   header += "<h5>Results</h5>";
-  // content += "<tr><th>Senate District:</th><td>"+featureProperties['District']+"</td></tr>"
-  // content += "<tr><th>Senator:</th><td>"+featureProperties['name']+"</td></tr>"
 
-for (attributes in featureProperties){
-              if( attributes.match(/MNSENDIST/gi) || attributes.match(/OBJECTID/gi) || attributes.match(/Shape_Area/gi) || attributes.match(/Shape_Leng/gi) || attributes.match(/SENDIST/gi) || attributes.match(/memid/gi) || attributes.match(/name/gi) || attributes.match(/party/gi)){
-                content += "";
-              }else{
-                // console.log(attributes, featureProperties[attributes])
-                content += "<tr><th>"+attributes+":</th><td>"+featureProperties[attributes]+"</td></tr>"
-              }   
-            }
-
-// switch (activeSelect.geography) {
-//     case "cng":
-//           for (attributes in featureProperties){
-//               if( attributes.match(/MNSENDIST/gi) || attributes.match(/OBJECTID/gi) || attributes.match(/Shape_Area/gi) || attributes.match(/Shape_Leng/gi) || attributes.match(/district/gi) || attributes.match(/SENDIST/gi) || attributes.match(/memid/gi) || attributes.match(/name/gi) || attributes.match(/party/gi)){
-//                 content += "";
-//               }else{
-//                 // console.log(attributes, featureProperties[attributes])
-//                 content += "<tr><th>"+attributes+":</th><td>"+featureProperties[attributes]+"</td></tr>"
-//               }   
-//             }
-//             break;
-
-
-//     }
+  for (attributes in featureProperties){
+    if( attributes.match(/MNSENDIST/gi) || attributes.match(/OBJECTID/gi) || attributes.match(/Shape_Area/gi) || attributes.match(/Shape_Leng/gi) || attributes.match(/SENDIST/gi) || attributes.match(/memid/gi) || attributes.match(/name/gi) || attributes.match(/party/gi)){
+      content += "";
+    }else{
+      // console.log(attributes, featureProperties[attributes])
+      content += "<tr><th>"+attributes+":</th><td>"+featureProperties[attributes]+"</td></tr>"
+    }   
+  }
 
   $("#results").html(content);
-  // district += feature.properties.SENDIST
-  // content += "<tr><th>Total Votes: </th><td>"+feature[activeSelect.selection+'TOTAL'].toLocaleString()+"</td></tr>";
-
 }
 
 function removeLayers(c){
@@ -172,29 +136,8 @@ function removeLayers(c){
         map.setFilter(activeSelect.geography+"-highlighted-"+activeSelect.year, ['all', ['==', 'Year', activeSelect.year], ['==', 'FeatType', activeSelect.geography], ["in", "District", ""]])
 
         $('#results').html("");
-        // $('#precinct-results').html("");
-        $('#clear').hide();
-
-        // if(activeTab.selection == 'USPRS' || activeTab.selection == 'USSEN'){
-        //   $('#candidate-table').show();
-        // } else{
-        //   $('#candidate-table').hide();
-        // }
-    //remove old pushpin and previous selected district layers 
-    if (typeof map.getSource('pointclick') !== "undefined" ){ 
-      // console.log('remove previous marker');
-      map.removeLayer('pointclick');    
-      map.removeSource('pointclick');
-    }   
+        $('#clear').hide(); 
     break;    
-    case 'pushpin':
-    //remove old pushpin and previous selected district layers 
-    if (typeof map.getSource('pointclick') !== "undefined" ){ 
-      // console.log('remove previous marker');
-      map.removeLayer('pointclick');    
-      map.removeSource('pointclick');
-    }
-    break;
   }    
 }
 
