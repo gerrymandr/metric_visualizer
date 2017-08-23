404e4449f9425499871223904d2af5e333668fa5
solbrigm <solbrigm@gmail.com>
2017-08-23 12:15:11 -0700

    Updated to accept arguments Refactored data_downloader to only download data. Moved the project-specific GerryData object into GerryMetrics.py, which I will work on next.
    Issue: Still need to make sure it respects the 'out' argument and does not just write to current working directory.

---
+++ data_scripts/GerryMetrics.py
@@ -0,0 +1,55 @@
+class GerryData:
+    """
+
+    """
+
+    def __init__(self, root='.'):
+        print('root directory is {0}'.format(os.path.abspath(root)))
+        self.root = root
+        self.raw_dir = os.path.join(self.root, 'RawData')
+        self.extracted_dir = os.path.join(self.root, 'ExtractedData')
+        self.clean_directory()
+
+    def clean_directory(self):
+        shutil.rmtree(self.raw_dir, ignore_errors=True)
+        shutil.rmtree(self.extracted_dir, ignore_errors=True)
+        os.mkdir(self.raw_dir)
+        os.mkdir(self.extracted_dir)
+
+    def download_file(self, file_name, url):
+        """Downloads file_name from base_url.file_name.zip, downloads to folder RawData in root"""
+
+        destination_dir = os.path.join(self.root, 'RawData')
+        download_file(destination_dir, file_name, url)
+
+    def down_files(self, file_list):
+        """Downloads a list of files structured as filename, url"""
+        p = Pool(1)
+        p.map(_download_file, file_list)
+
+    def process_raw(self):
+        """"
+        1. Extract only desired columns
+        2. Rename column in uniform manner
+        3. Re-project to 4326
+        """
+        shps = []
+        for root, dirs, files in os.walk('RawData'):
+            for file in files:
+                if file.endswith('.shp'):
+                    shps.append(file)
+
+        for shp in shps:
+            f_out = os.path.join(self.root, 'ExtractedData', os.path.basename(shp))
+            self.process_file(shp, f_out, {})
+
+    @staticmethod
+    def process_file(f_in, f_out, field_dictionary):
+        """Takes in a file in_path, extracts fields based on field_dicionary,
+
+        :param file: .shp file
+        :param field_dictionary: dictionary of field names making explicit
+        :return:
+        """
+
+        print(f_out)
--- data_scripts/data_downloader.py
+++ data_scripts/data_downloader.py
@@ -1,3 +1,14 @@
+"""
+Downloads and uncompresses data from a list of urls.
+
+File to read from must be a .csv with no headers, formatted as follows:
+<FileName>,<url>
+<FileName1>,<url1>
+<FileName2>,<url2>
+
+See all.csv for an example
+"""
+
 import shutil
 import sys
 import os
@@ -15,7 +26,13 @@ else:
 
 
 def download_file(out_dir, file_name, url):
-    """Downloads file_name from base_url.file_name.zip, downloads to folder RawData in root"""
+    """Downloads file from url and extracts into out_dir
+
+    :param out_dir: Directory to write to
+    :param file_name: Name of file, including file extension.
+    :param url: Path to download from
+    :return: None
+    """
 
     if not os.path.exists(out_dir):
         print('Creating new directory {}'.format(out_dir))
@@ -29,97 +46,58 @@ def download_file(out_dir, file_name, url):
     if split_name[1] == '.zip':
         zip_ref = zipfile.ZipFile(filehandle, 'r')
         zip_ref.extractall(os.path.join(out_dir, split_name[0]))
-    if split_name[1] == '.gz':
+    elif split_name[1] == '.gz':
         tar_ref = tarfile.open(filehandle, 'r:gz')
         tar_ref.extractall(os.path.join(out_dir, split_name[0]))
+    else:
+        os.rename(filehandle, os.path.join(out_dir, file_name))
 
 
 def _download_file(x):
-    """A wrapper for download_file that allows it to be used with multiprocessing"""
+    """A wrapper for download_file that allows it to be used with multiprocessing
+
+    :param x: A dictionary with keys out_dir, file_name, url to be passed to download_file
+    :return: None
+    """
     download_file('./RawData', *x)
 
 
 def download_files(file_list):
-    """Downloads a list of files structured as filename, url"""
+    """Downloads a list of files structured as filename, url
+
+    :param file_list: A list of dictionaries with keys out_dir, file_name, url to be passed to download_file
+    :return: None
+    """
     p = Pool()
     p.map(_download_file, file_list)
 
 
-class GerryData:
-    """
+def clean_directory(root):
+    directory = os.path.join(root, 'RawData')
+    shutil.rmtree(directory, ignore_errors=True)
+    os.mkdir(directory)
 
-    """
-
-    def __init__(self, root='.'):
-        print('root directory is {0}'.format(os.path.abspath(root)))
-        self.root = root
-        self.raw_dir = os.path.join(self.root, 'RawData')
-        self.extracted_dir = os.path.join(self.root, 'ExtractedData')
-        self.clean_directory()
-
-    def clean_directory(self):
-        shutil.rmtree(self.raw_dir, ignore_errors=True)
-        shutil.rmtree(self.extracted_dir, ignore_errors=True)
-        os.mkdir(self.raw_dir)
-        os.mkdir(self.extracted_dir)
-
-    def download_file(self, file_name, url):
-        """Downloads file_name from base_url.file_name.zip, downloads to folder RawData in root"""
-
-        destination_dir = os.path.join(self.root, 'RawData')
-        download_file(destination_dir, file_name, url)
-
-    def down_files(self, file_list):
-        """Downloads a list of files structured as filename, url"""
-        p = Pool(1)
-        p.map(_download_file, file_list)
-
-    def process_raw(self):
-        """"
-        1. Extract only desired columns
-        2. Rename column in uniform manner
-        3. Re-project to 4326
-        """
-        shps = []
-        for root, dirs, files in os.walk('RawData'):
-            for file in files:
-                if file.endswith('.shp'):
-                    shps.append(file)
-
-        for shp in shps:
-            f_out = os.path.join(self.root, 'ExtractedData', os.path.basename(shp))
-            self.process_file(shp, f_out, {})
-
-    @staticmethod
-    def process_file(f_in, f_out, field_dictionary):
-        """Takes in a file in_path, extracts fields based on field_dicionary,
-
-        :param file: .shp file
-        :param field_dictionary: dictionary of field names making explicit
-        :return:
-        """
-
-        print(f_out)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Extacts fils from list of urls')
     parser.add_argument('--out', default='.',
                         help='where to write files to (default: current working directory)')
-    parser.add_argument('--file_list', default='./url_list.csv',
+    parser.add_argument('--file_list', default='./all.csv',
                         help='List of file names and urls to download. '
                              'Format: <FileName>,<url> '
-                             'no spaces, no quotation marks. (default: ./url_list.csv')
+                             'no spaces, no quotation marks. (default: ./all.csv')
 
     args = parser.parse_args()
+    root_dir = args.out
 
-    downloader = GerryData(args.out)
+    clean_directory(root_dir)
 
     with open(args.file_list, 'r') as f:
         file_urls = list(csv.reader(f))
 
     download_files(file_urls)
 
-    #foo.process_raw()
-    #os.rename(os.path.join(foo.raw_dir, 'C2002', 'C2002.DBF'),
-    #          os.path.join(foo.raw_dir, 'C2002', 'c2002.dbf'))
+    # Fixes file names from the default datasource, allowing ogr tools to read it.
+    #os.rename(os.path.join(downloader.raw_dir, 'C2002', 'C2002.DBF'),
+    #          os.path.join(downloader.raw_dir, 'C2002', 'c2002.dbf'))
